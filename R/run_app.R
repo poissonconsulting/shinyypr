71f61f63f35c9538d5a90a67adb40053dd1bfa6c
Nadine Hussein <nadinehussein3@gmail.com>
2021-09-10 15:59:23 -0600

    replace chkor

--- R/run_app.R
+++ R/run_app.R
@@ -5,8 +5,10 @@
 #' @examples
 #' if(interactive()){run_ypr_app()}
 run_ypr_app <- function(population = NULL) {
-  chk::chkor(chk_population(population, parameters), chk::chk_null(population))
-
+  
+  if(!is.null(population)) {
+    chk_population(population, parameters)
+}
   shinyOptions(population = population)
 
   shiny::shinyAppDir(system.file("app", package = "shinyypr"), 
